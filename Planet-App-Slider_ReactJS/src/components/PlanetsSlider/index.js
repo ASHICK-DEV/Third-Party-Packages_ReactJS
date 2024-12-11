cce9ea41aa1cea951a92e86e30d3ca12937ac968
ASHICK <rainaashick231173@gmail.com>
2024-12-11 07:23:09 +0000

    Planet App Using Slider

---
+++ Planet-App-Slider_ReactJS/src/components/PlanetsSlider/index.js
@@ -0,0 +1,23 @@
+import Slider from 'react-slick'
+
+import PlanetItem from '../PlanetItem'
+
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div className="planets-app-container" data-testid="planets">
+      <h1 className="heading">PLANETS</h1>
+      <p className="own">Designed by @Ashiq</p>
+      <Slider>
+        {planetsList.map(eachPlanet => (
+          <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
